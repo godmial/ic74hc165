b88993e6578b1052507a87782a56cd22f273d820
吴英祥 <2633967641@qq.com>
2025-05-19 13:57:41 +0800

    优化配置信息

--- 74165_platform.c
+++ 74165_platform.c
@@ -1,150 +1,134 @@
 /**
  **********************************************************************************
  * @file   74165_platform.c
- * @author Hossein.M (https://github.com/Hossein-M98)
- * @brief  A sample Platform dependent layer for 74165 Driver
- **********************************************************************************
- *
- * MIT License
+ * @brief  Platform-dependent implementation for 74165 Driver
  **********************************************************************************
  */
 
-/* Includes ---------------------------------------------------------------------*/
-#include "74165_platform.h"
-#include <rtthread.h>
-#include <rtdevice.h>
-
-/* Private GPIO helpers --------------------------------------------------------*/
-static void IC74165_SetGPIO_OUT(rt_base_t GPIO_Pin)
-{
-    rt_pin_mode(GPIO_Pin, PIN_MODE_OUTPUT);
-}
-
-static void IC74165_SetGPIO_IN(rt_base_t GPIO_Pin)
-{
-    rt_pin_mode(GPIO_Pin, PIN_MODE_INPUT);
-}
-
-#ifdef PKG_IC74HC165_GPIO_MODE
-/* ====================== GPIO 模式实现 ========================== */
-
-static void IC74165_PlatformInit(void)
-{
-#if (IC74165_CLKINH_ENABLE)
-    IC74165_SetGPIO_OUT(CE_PIN);
-#endif
-    IC74165_SetGPIO_OUT(CP_PIN);
-    IC74165_SetGPIO_OUT(PL_PIN);
-    IC74165_SetGPIO_IN(Q7_PIN);
-}
-
-static void IC74165_PlatformDeInit(void)
-{
-    // 可选：清空引脚配置
-}
-
-#if (IC74165_CLKINH_ENABLE)
-static void IC74165_ClkInhWrite(uint8_t Level)
-{
-    rt_pin_write(CE_PIN, Level);
-}
-#endif
-
-static void IC74165_ClkWrite(uint8_t Level)
-{
-    rt_pin_write(CP_PIN, Level);
-}
-
-static void IC74165_ShLdWrite(uint8_t Level)
-{
-    rt_pin_write(PL_PIN, Level);
-}
-
-static uint8_t IC74165_QhRead(void)
-{
-    return rt_pin_read(Q7_PIN);
-}
-
-static void IC74165_DelayUs(uint8_t Delay)
-{
-    rt_hw_us_delay(Delay);
-}
-
-/**
- * @brief 初始化 GPIO 模式的平台封装结构
- */
-void IC74165_Platform_Init(IC74165_Handler_t *Handler)
-{
-    IC74165_PLATFORM_SET_COMMUNICATION(Handler, IC74165_COMMUNICATION_GPIO);
-    IC74165_PLATFORM_LINK_INIT(Handler, IC74165_PlatformInit);
-    IC74165_PLATFORM_LINK_DEINIT(Handler, IC74165_PlatformDeInit);
-#if (IC74165_CLKINH_ENABLE)
-    IC74165_PLATFORM_LINK_CLKINHWRITE(Handler, IC74165_ClkInhWrite);
-#endif
-    IC74165_PLATFORM_LINK_GPIO_CLKWRITE(Handler, IC74165_ClkWrite);
-    IC74165_PLATFORM_LINK_GPIO_SHLDWRITE(Handler, IC74165_ShLdWrite);
-    IC74165_PLATFORM_LINK_GPIO_QHREAD(Handler, IC74165_QhRead);
-    IC74165_PLATFORM_LINK_GPIO_DELAYUS(Handler, IC74165_DelayUs);
-}
-#endif /* PKG_IC74HC165_GPIO_MODE */
-
-#ifdef PKG_IC74HC165_SPI_MODE
-/* ====================== SPI 模式实现 ========================== */
-
-static struct rt_spi_device *spi_dev = RT_NULL;
-
-static void IC74165_PlatformInit_SPI(void)
-{
-    spi_dev = (struct rt_spi_device *)rt_device_find("spi10");
-    if (spi_dev == RT_NULL)
-    {
-        rt_kprintf("Error: SPI device not found.\n");
-        return;
-    }
-
-    struct rt_spi_configuration cfg = {
-        .mode = RT_SPI_MASTER | RT_SPI_MODE_1 | RT_SPI_MSB,
-        .max_hz = 3000000,
-        .data_width = 8,
-    };
-    rt_spi_configure(spi_dev, &cfg);
-
-#if (IC74165_CLKINH_ENABLE)
-    IC74165_SetGPIO_OUT(CE_PIN);
-#endif
-}
-
-static void IC74165_PlatformDeInit_SPI(void)
-{
-    spi_dev = RT_NULL;
-}
-
-static void IC74165_SPI_SendReceive(uint8_t *SendData, uint8_t *ReceiveData, uint8_t Len)
-{
-    uint8_t TxBuff[SPI_MAXIMUM_BUFFER_SIZE] = {0xFF};
-    struct rt_spi_message msg = {
-        .send_buf = SendData ? SendData : TxBuff,
-        .recv_buf = ReceiveData,
-        .length = Len
-    };
-
-    if (rt_spi_transfer_message(spi_dev, &msg) != Len)
-    {
-        rt_kprintf("spi transfer failed!\n");
-    }
-}
-
-/**
- * @brief 初始化 SPI 模式的平台封装结构
- */
-void IC74165_Platform_Init_SPI(IC74165_Handler_t *Handler)
-{
-    IC74165_PLATFORM_SET_COMMUNICATION(Handler, IC74165_COMMUNICATION_SPI);
-    IC74165_PLATFORM_LINK_INIT(Handler, IC74165_PlatformInit_SPI);
-    IC74165_PLATFORM_LINK_DEINIT(Handler, IC74165_PlatformDeInit_SPI);
-#if (IC74165_CLKINH_ENABLE)
-    IC74165_PLATFORM_LINK_CLKINHWRITE(Handler, IC74165_ClkInhWrite);
-#endif
-    IC74165_PLATFORM_LINK_SPI_SENDRECEIVE(Handler, IC74165_SPI_SendReceive);
-}
-#endif /* PKG_IC74HC165_SPI_MODE */
+ #include "74165_platform.h"
+ #include <rtthread.h>
+ #include <rtdevice.h>
+ 
+ static void IC74165_SetGPIO_OUT(rt_base_t GPIO_Pin)
+ {
+     rt_pin_mode(GPIO_Pin, PIN_MODE_OUTPUT);
+ }
+ 
+ static void IC74165_SetGPIO_IN(rt_base_t GPIO_Pin)
+ {
+     rt_pin_mode(GPIO_Pin, PIN_MODE_INPUT);
+ }
+ 
+ #ifdef PKG_IC74HC165_GPIO_MODE
+ 
+ static void IC74165_PlatformInit(void)
+ {
+ #if (IC74165_CLKINH_ENABLE)
+     IC74165_SetGPIO_OUT(CE_PIN);
+ #endif
+     IC74165_SetGPIO_OUT(CP_PIN);
+     IC74165_SetGPIO_OUT(PL_PIN);
+     IC74165_SetGPIO_IN(Q7_PIN);
+ }
+ 
+ static void IC74165_PlatformDeInit(void) {}
+ 
+ #if (IC74165_CLKINH_ENABLE)
+ static void IC74165_ClkInhWrite(uint8_t Level)
+ {
+     rt_pin_write(CE_PIN, Level);
+ }
+ #endif
+ 
+ static void IC74165_ClkWrite(uint8_t Level)
+ {
+     rt_pin_write(CP_PIN, Level);
+ }
+ 
+ static void IC74165_ShLdWrite(uint8_t Level)
+ {
+     rt_pin_write(PL_PIN, Level);
+ }
+ 
+ static uint8_t IC74165_QhRead(void)
+ {
+     return rt_pin_read(Q7_PIN);
+ }
+ 
+ static void IC74165_DelayUs(uint8_t Delay)
+ {
+     rt_hw_us_delay(Delay);
+ }
+ 
+ void IC74165_Platform_Init(IC74165_Handler_t *Handler)
+ {
+     IC74165_PLATFORM_SET_COMMUNICATION(Handler, IC74165_COMMUNICATION_GPIO);
+     IC74165_PLATFORM_LINK_INIT(Handler, IC74165_PlatformInit);
+     IC74165_PLATFORM_LINK_DEINIT(Handler, IC74165_PlatformDeInit);
+ #if (IC74165_CLKINH_ENABLE)
+     IC74165_PLATFORM_LINK_CLKINHWRITE(Handler, IC74165_ClkInhWrite);
+ #endif
+     IC74165_PLATFORM_LINK_GPIO_CLKWRITE(Handler, IC74165_ClkWrite);
+     IC74165_PLATFORM_LINK_GPIO_SHLDWRITE(Handler, IC74165_ShLdWrite);
+     IC74165_PLATFORM_LINK_GPIO_QHREAD(Handler, IC74165_QhRead);
+     IC74165_PLATFORM_LINK_GPIO_DELAYUS(Handler, IC74165_DelayUs);
+ }
+ #endif /* PKG_IC74HC165_GPIO_MODE */
+ 
+ #ifdef PKG_IC74HC165_SPI_MODE
+ 
+ static struct rt_spi_device *spi_dev = RT_NULL;
+ 
+ static void IC74165_PlatformInit_SPI(void)
+ {
+     spi_dev = (struct rt_spi_device *)rt_device_find("spi10");
+     if (spi_dev == RT_NULL)
+     {
+         rt_kprintf("Error: SPI device not found.\n");
+         return;
+     }
+ 
+     struct rt_spi_configuration cfg = {
+         .mode = RT_SPI_MASTER | RT_SPI_MODE_1 | RT_SPI_MSB,
+         .max_hz = 3000000,
+         .data_width = 8,
+     };
+     rt_spi_configure(spi_dev, &cfg);
+ 
+ #if (IC74165_CLKINH_ENABLE)
+     IC74165_SetGPIO_OUT(CE_PIN);
+ #endif
+ }
+ 
+ static void IC74165_PlatformDeInit_SPI(void)
+ {
+     spi_dev = RT_NULL;
+ }
+ 
+ static void IC74165_SPI_SendReceive(uint8_t *SendData, uint8_t *ReceiveData, uint8_t Len)
+ {
+     uint8_t TxBuff[SPI_MAXIMUM_BUFFER_SIZE] = {0xFF};
+     struct rt_spi_message msg = {
+         .send_buf = SendData ? SendData : TxBuff,
+         .recv_buf = ReceiveData,
+         .length = Len
+     };
+ 
+     if (rt_spi_transfer_message(spi_dev, &msg) != Len)
+     {
+         rt_kprintf("spi transfer failed!\n");
+     }
+ }
+ 
+ void IC74165_Platform_Init_SPI(IC74165_Handler_t *Handler)
+ {
+     IC74165_PLATFORM_SET_COMMUNICATION(Handler, IC74165_COMMUNICATION_SPI);
+     IC74165_PLATFORM_LINK_INIT(Handler, IC74165_PlatformInit_SPI);
+     IC74165_PLATFORM_LINK_DEINIT(Handler, IC74165_PlatformDeInit_SPI);
+ #if (IC74165_CLKINH_ENABLE)
+     IC74165_PLATFORM_LINK_CLKINHWRITE(Handler, IC74165_ClkInhWrite);
+ #endif
+     IC74165_PLATFORM_LINK_SPI_SENDRECEIVE(Handler, IC74165_SPI_SendReceive);
+ }
+ #endif /* PKG_IC74HC165_SPI_MODE */
+ 
--- 74165_platform.h
+++ 74165_platform.h
@@ -1,30 +1,8 @@
 /**
  **********************************************************************************
  * @file   74165_platform.h
- * @author Hossein.M (https://github.com/Hossein-M98)
+ * @author Hossein.M
  * @brief  A sample Platform dependent layer for 74165 Driver
- **********************************************************************************
- *
- * Copyright (c) 2021 Hossein.M (MIT License)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- *
  **********************************************************************************
  */
 
@@ -43,13 +21,12 @@
  #define IC74165_CLKINH_ENABLE 1
  
  /**
-  * @brief  Specify IO Pins of STM32 connected to 74165
-  *         可通过 Kconfig 配置
+  * @brief  固定的 IO 引脚定义
   */
- #define CP_PIN GET_PIN(CONFIG_IC74165_CP_PORT, CONFIG_IC74165_CP_PIN)   // CLK
- #define PL_PIN GET_PIN(CONFIG_IC74165_PL_PORT, CONFIG_IC74165_PL_PIN)   // SH/LD
- #define Q7_PIN GET_PIN(CONFIG_IC74165_Q7_PORT, CONFIG_IC74165_Q7_PIN)   // Q7
- #define CE_PIN GET_PIN(CONFIG_IC74165_CE_PORT, CONFIG_IC74165_CE_PIN)   // CLK_INH
+ #define CP_PIN GET_PIN(A, 5)   // CLK
+ #define PL_PIN GET_PIN(A, 7)   // SH/LD
+ #define Q7_PIN GET_PIN(A, 6)   // Q7
+ #define CE_PIN GET_PIN(A, 8)   // CLK_INH
  
  #define SPI_MAXIMUM_BUFFER_SIZE 64
  
@@ -57,20 +34,7 @@
  extern "C" {
  #endif
  
- /**
-  * @brief  Initialize platform dependent layer to communicate with 74165 using
-  *         GPIO.
-  * @param  Handler: Pointer to handler
-  * @retval None
-  */
  void IC74165_Platform_Init(IC74165_Handler_t *Handler);
- 
- /**
-  * @brief  Initialize platform dependent layer to communicate with 74165 using
-  *         SPI.
-  * @param  Handler: Pointer to handler
-  * @retval None
-  */
  void IC74165_Platform_Init_SPI(IC74165_Handler_t *Handler);
  
  #ifdef __cplusplus
